f9b4692de8d0b535b2f16a9a2723d2f5e26eb05b
yamatsum <sy12250@gmail.com>
2024-09-09 15:28:19 +0900

    fix: tsserver to ts_ls

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -8,7 +8,7 @@ local M = {
 function M.config()
   require("mason").setup()
   require("mason-lspconfig").setup {
-    ensure_installed = { "rust_analyzer", "tsserver", "denols", "lua_ls" },
+    ensure_installed = { "ts_ls", "lua_ls" },
     automatic_installation = true,
   }
 end
