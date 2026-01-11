0c8a63fb5f269b18cb2364517a641f454e874e43
ya2s <sy12250@gmail.com>
2026-01-11 12:37:20 +0900

    Comment out custom Treesitter filetype config
    The custom filetype list and related Treesitter installation and autocmd logic have been commented out. This simplifies the plugin configuration and may defer to default behavior.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,19 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
-  branch = 'main',
   build = ':TSUpdate',
   config = function()
-    local tree_sitter_filetypes = {
-      "json",
-      "lua",
-      "typescript",
-      "tsx",
-    }
+    -- local tree_sitter_filetypes = {
+    -- "json",
+    -- "lua",
+    -- "typescript",
+    -- "tsx",
+    -- }
 
-    require 'nvim-treesitter'.install(tree_sitter_filetypes)
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = tree_sitter_filetypes,
-      callback = function() vim.treesitter.start() end,
-    })
+    -- require('nvim-treesitter').install(tree_sitter_filetypes):wait(300000)
+    -- vim.api.nvim_create_autocmd('FileType', {
+    -- pattern = tree_sitter_filetypes,
+    -- callback = function() vim.treesitter.start() end,
+    -- })
   end,
 }
