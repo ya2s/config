baf00a090ea3745d28a3c5131f85f6ec8a5aab96
ya2s <sy12250@gmail.com>
2026-03-30 11:50:09 +0900

    Move Telescope setup to runtime plugin
    Remove the legacy lua_old Telescope config and add a runtime plugin at nvim/plugin/telescope.lua. The new plugin registers plenary and telescope via vim.pack.add (skipped in VSCode), calls require('telescope').setup with UI/layout defaults, and binds <leader>p and <leader>f in normal mode to git_files and live_grep.

---
+++ nvim/plugin/telescope.lua
@@ -0,0 +1,22 @@
+if not vim.g.vscode then
+  vim.pack.add({
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/nvim-telescope/telescope.nvim",
+  })
+
+  require("telescope").setup({
+    defaults = {
+      layout_config = {
+        prompt_position = "top",
+      },
+      prompt_prefix = "   ",
+      selection_caret = " ❯ ",
+      entry_prefix = "   ",
+      sorting_strategy = "ascending",
+      winblend = 20,
+    },
+  })
+
+  vim.keymap.set("n", "<leader>p", function() require("telescope.builtin").git_files() end, { silent = true })
+  vim.keymap.set("n", "<leader>f", function() require("telescope.builtin").live_grep() end, { silent = true })
+end
