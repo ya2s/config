d4e4e09bdf04befe3a6991029c74ed7ce4eac924
ya2s <sy12250@gmail.com>
2026-04-02 13:18:53 +0900

    Use zimfw for zsh plugins; clean aliases & PATH
    Migrate zsh plugin handling to zimfw: add zsh/zimrc with core modules (completion, zsh-completions, zsh-autosuggestions, fast-syntax-highlighting), download/init zimfw in plugs.zsh, and source zim init. Remove old manual plug lines and compinit usage. Tidy up shell config by removing a few aliases (k, kc) and several PATH entries and a Google Cloud completion source from path.zsh to centralize and simplify environment setup.

--- zsh/plugs.zsh
+++ zsh/plugs.zsh
@@ -1,9 +1,13 @@
-[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
+ZIM_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/zim"
+ZIM_CONFIG_FILE="$XDG_CONFIG_HOME/zsh/zimrc"
 
-plug "zsh-users/zsh-autosuggestions"
-plug "zsh-users/zsh-syntax-highlighting"
-plug "zsh-users/zsh-completions"
-plug "paulirish/git-open"
-plug "atoftegaard-git/zsh-omz-autocomplete"
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+    https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+fi
 
-autoload -Uz compinit && compinit
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE} ]]; then
+  source ${ZIM_HOME}/zimfw.zsh init -q
+fi
+
+source ${ZIM_HOME}/init.zsh
