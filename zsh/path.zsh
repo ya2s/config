854b79fc0dedd9147f4d7e7606dc0a1c1e33b6b3
ya2s <sy12250@gmail.com>
2026-04-02 20:56:04 +0900

    Replace Starship with Pure prompt and tweaks
    Migrate from Starship to the Pure prompt: remove starship config and related STARSHIP_CONFIG export and init, add sindresorhus/pure zmodule and customize Pure prompt colors via zstyle. Also remove pnpm completion file, update zsh-autosuggestions highlight color, and add a new git alias (open = !gh browse). Minor path and zimfw bootstrap unchanged.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,2 @@
 export NVIM_ROOT="$XDG_CONFIG_HOME/nvim"
-export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/config.toml
 export PATH="$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin"
--- zsh/plugs.zsh
+++ zsh/plugs.zsh
@@ -1,6 +1,21 @@
 ZIM_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/zim"
 ZIM_CONFIG_FILE="$XDG_CONFIG_HOME/zsh/zimrc"
 
+# pure prompt colors (primer dark)
+zstyle ':prompt:pure:path' color '#B7BDC8'
+zstyle ':prompt:pure:git:branch' color '#656C76'
+zstyle ':prompt:pure:git:dirty' color '#D29922'
+zstyle ':prompt:pure:git:arrow' color '#4493F8'
+zstyle ':prompt:pure:git:stash' color '#656C76'
+zstyle ':prompt:pure:git:action' color '#F85149'
+zstyle ':prompt:pure:prompt:success' color '#4493F8'
+zstyle ':prompt:pure:prompt:error' color '#F85149'
+zstyle ':prompt:pure:host' color '#656C76'
+zstyle ':prompt:pure:user' color '#656C76'
+zstyle ':prompt:pure:virtualenv' color '#656C76'
+zstyle ':prompt:pure:prompt:continuation' color '#3D444D'
+zstyle ':prompt:pure:execution_time' color '#656C76'
+
 if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
   curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
     https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
